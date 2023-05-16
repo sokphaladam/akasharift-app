4d8e291f07432efde313d77c2466970d3daf61eb
Sokphal Adam <sokphaladam@gmail.com>
2023-05-17 01:48:47 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
 import { RenderHomeScreen } from "../src/screens/RenderHomeScreen";
+import Error from "next/error";
 
-const Home: NextPage = () => {
+export async function getServerSideProps() {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const errorCode = res.ok ? false : res.status;
+  const json = await res.json();
+
+  return {
+    props: { errorCode, stars: json.stargazers_count },
+  };
+}
+
+const Home: NextPage = ({ errorCode, stars }: any) => {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />;
+  }
+
+  console.log(stars);
+
   return <RenderHomeScreen />;
 };
 
